dce77ae78491d61d312faa35cbdcb35bb0bd5f95
Anand raja <meetanandns@gmail.com>
2022-08-30 15:29:22 +0000

    basic working html server

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'index.js'
+  },
+  devServer: {
+    allowedHosts: [
+      'all'
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html'
+    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     { from: './src/main.wasm', to: './' }, // output path is 'public' folder, already defined.
+    //     { from: './src/styles.css', to: './' }
+    //   ]
+    // })
+  ]
+}
